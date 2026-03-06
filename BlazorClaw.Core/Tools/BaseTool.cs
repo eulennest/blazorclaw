fa6ce201023cec35f5dbb8021aaaa946a2333e86
Kaspar Kastl <kaspar@eulencode.de>
2026-03-06 02:51:48 +0000

    feat(core): Add ToolContext to ITool interface and BaseTool

--- BlazorClaw.Core/Tools/BaseTool.cs
+++ BlazorClaw.Core/Tools/BaseTool.cs
@@ -1,14 +1,23 @@
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
+using Microsoft.AspNetCore.Http;
 
 namespace BlazorClaw.Core.Tools;
+
+public class ToolContext
+{
+    public Guid SessionId { get; set; }
+    public HttpContext? HttpContext { get; set; }
+    public string UserId { get; set; } = string.Empty;
+    public IServiceProvider ServiceProvider { get; set; } = default!;
+}
 
 public interface ITool
 {
     string Name { get; }
     string Description { get; }
     object GetSchema(); 
-    Task<string> ExecuteAsync(string argumentsJson);
+    Task<string> ExecuteAsync(string argumentsJson, ToolContext context);
 }
 
 public abstract class BaseTool<TParams> : ITool where TParams : class
@@ -16,9 +25,9 @@ public abstract class BaseTool<TParams> : ITool where TParams : class
     public abstract string Name { get; }
     public abstract string Description { get; }
     
-    public override object GetSchema() => SchemaGenerator.Generate(typeof(TParams));
+    public object GetSchema() => SchemaGenerator.Generate(typeof(TParams));
 
-    public async Task<string> ExecuteAsync(string argumentsJson)
+    public async Task<string> ExecuteAsync(string argumentsJson, ToolContext context)
     {
         var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
         var deserializedParams = JsonSerializer.Deserialize<TParams>(argumentsJson, options);
@@ -30,8 +39,8 @@ public abstract class BaseTool<TParams> : ITool where TParams : class
         var validationContext = new ValidationContext(deserializedParams);
         Validator.ValidateObject(deserializedParams, validationContext, validateAllProperties: true);
         
-        return await ExecuteInternalAsync(deserializedParams);
+        return await ExecuteInternalAsync(deserializedParams, context);
     }
 
-    protected abstract Task<string> ExecuteInternalAsync(TParams parameters);
+    protected abstract Task<string> ExecuteInternalAsync(TParams parameters, ToolContext context);
 }
