058cf2da54cb4eca7ac83bc64bdc050fd585e717
Kaspar Kastl <kaspar@eulencode.de>
2026-03-06 18:23:55 +0000

    feat(Telegram): add TelegramBotHostedService (work-in-progress, build fix needed)

--- BlazorClaw.Server/Services/TelegramBotHostedService.cs
+++ BlazorClaw.Server/Services/TelegramBotHostedService.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Identity;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Polling;
 using BlazorClaw.Server.Data;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace BlazorClaw.Server.Services
 {
@@ -20,7 +22,7 @@ namespace BlazorClaw.Server.Services
             _serviceProvider = serviceProvider;
         }
 
-        public async Task StartAsync(CancellationToken cancellationToken)
+        public Task StartAsync(CancellationToken cancellationToken)
         {
             var telegramConfigs = _configuration.GetSection("Channels:Telegram").GetChildren();
             foreach (var botConfig in telegramConfigs)
@@ -32,38 +34,42 @@ namespace BlazorClaw.Server.Services
                     var client = new TelegramBotClient(token);
                     _bots.Add(new TelegramBotInstance(id, token, client));
                     
-                    // Start Polling (non-blocking)
+                    var receiverOptions = new ReceiverOptions
+                    {
+                        AllowedUpdates = Array.Empty<Telegram.Bot.Types.Enums.UpdateType>() // receive all update types
+                    };
+
                     client.StartReceiving(
-                        updateHandler: HandleUpdateAsync,
-                        pollingErrorHandler: (c, e, t) => Task.CompletedTask,
+                        updateHandler: async (bot, update, token) => await HandleUpdateAsync(bot, update, token),
+                        pollingErrorHandler: async (bot, exception, token) => await HandlePollingErrorAsync(bot, exception, token),
+                        receiverOptions: receiverOptions,
                         cancellationToken: cancellationToken
                     );
                 }
             }
+            return Task.CompletedTask;
+        }
+
+        private Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
         }
 
         private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            var logger = _serviceProvider.GetRequiredService<ILogger<TelegramBotHostedService>>();
-            logger.LogInformation("Telegram Update received: {Type}", update.Type);
-
             if (update.Message?.Text == null) return;
 
             using var scope = _serviceProvider.CreateScope();
             var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
             
             var telegramId = update.Message.From!.Id.ToString();
-            logger.LogInformation("Looking up user for Telegram ID: {TelegramId}", telegramId);
-            
-            // Suche User via Provider "Telegram"
             var user = await userManager.FindByLoginAsync("Telegram", telegramId);
             
             string reply = user != null 
                 ? $"Hallo {user.FirstName}, ich habe dich erkannt!" 
                 : "Ich kenne dich leider noch nicht. Bitte registriere dich.";
 
-            logger.LogInformation("Sending reply to {ChatId}: {Reply}", update.Message.Chat.Id, reply);
-            await botClient.SendTextMessageAsync(update.Message.Chat.Id, reply, cancellationToken: cancellationToken);
+            await botClient.SendMessage(update.Message.Chat.Id, reply, cancellationToken: cancellationToken);
         }
 
         public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
