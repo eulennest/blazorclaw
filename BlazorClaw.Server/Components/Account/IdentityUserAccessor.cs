6f36f3c967da7d6d782f9296d39db7d49208989b
Daniel Rieper <git@qdaniel.de>
2026-03-06 12:28:27 +0100

    fix warnings

--- BlazorClaw.Server/Components/Account/IdentityUserAccessor.cs
+++ BlazorClaw.Server/Components/Account/IdentityUserAccessor.cs
@@ -5,7 +5,7 @@ namespace BlazorClaw.Server.Components.Account
 {
     internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
     {
-        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        public async Task<ApplicationUser?> GetRequiredUserAsync(HttpContext context)
         {
             var user = await userManager.GetUserAsync(context.User);
 
--- BlazorClaw.Server/Models/ChatSession.cs
+++ BlazorClaw.Server/Models/ChatSession.cs
@@ -9,28 +9,28 @@ public class ChatSession
     public Guid Id { get; set; } = Guid.NewGuid();
 
     public string Title { get; set; } = "Neuer Chat";
-    
-    public string CurrentModel { get; set; }
-    
+
+    public required string CurrentModel { get; set; }
+
     public string? ChannelId { get; set; }
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime LastUsedAt { get; set; } = DateTime.UtcNow;
-    
+
     public bool IsArchived { get; set; }
 
     // Navigation Property für Teilnehmer (Gruppen-Sitzungen)
-    public ICollection<ChatSessionParticipant> Participants { get; set; } = new List<ChatSessionParticipant>();
+    public ICollection<ChatSessionParticipant> Participants { get; set; } = [];
 }
 
 public class ChatSessionParticipant
 {
     [Key]
     public Guid Id { get; set; } = Guid.NewGuid();
-    
+
     public Guid SessionId { get; set; }
-    public ChatSession Session { get; set; }
-    
-    public string UserId { get; set; }
-    public ApplicationUser User { get; set; }
+    public virtual ChatSession? Session { get; set; }
+
+    public required string UserId { get; set; }
+    public virtual ApplicationUser? User { get; set; }
 }
