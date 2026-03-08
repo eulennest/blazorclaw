7edf4685de4f84f4bf703f4ddaadd2a2ee6d620c
Kaspar Kastl <kaspar@eulencode.de>
2026-03-08 19:15:08 +0000

    feat(Data): add ChannelRegisterToken and ChannelRegisterTokenExpiredAt to ApplicationUser

--- BlazorClaw.Core/Data/ApplicationUser.cs
+++ BlazorClaw.Core/Data/ApplicationUser.cs
@@ -7,4 +7,6 @@ public class ApplicationUser : IdentityUser
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public string? ChannelRegisterToken { get; set; }
+    public DateTime? ChannelRegisterTokenExpiredAt { get; set; }
 }
