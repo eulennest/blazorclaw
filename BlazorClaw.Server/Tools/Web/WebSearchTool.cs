ebde7d15489e3f439c6002ef4112f43d2b6de5b9
Kaspar Kastl <kaspar@eulencode.de>
2026-03-08 17:49:55 +0000

    feat(Tools): add WebSearchTool

--- BlazorClaw.Server/Tools/Web/WebSearchTool.cs
+++ BlazorClaw.Server/Tools/Web/WebSearchTool.cs
@@ -1,24 +1,33 @@
-using BlazorClaw.Core.Tools;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using BlazorClaw.Core.Tools;
+using BlazorClaw.Core.Web;
 
 namespace BlazorClaw.Server.Tools.Web;
 
-public class SearchParams
+public class WebSearchTool : BaseTool<WebSearchTool.Params>
 {
-    [Description("Suchbegriff")]
-    [Required]
-    public string Query { get; set; } = string.Empty;
-}
+    private readonly IWebSearchProvider _webSearchProvider;
 
-public class WebSearchTool : BaseTool<SearchParams>
-{
-    public override string Name => "web_search";
-    public override string Description => "Suche im Web via Brave API";
-
-    protected override Task<string> ExecuteInternalAsync(SearchParams p, ToolContext context)
+    public WebSearchTool(IWebSearchProvider webSearchProvider)
     {
-        // TODO: Brave Search API Integration (oder ähnlich)
-        return Task.FromResult($"Suche nach {p.Query} durchgeführt.");
+        _webSearchProvider = webSearchProvider;
+    }
+
+    public override string Name => "web_search";
+    public override string Description => "Sucht im Web nach Informationen.";
+
+    public class Params
+    {
+        [Required, Description("Suchbegriff")]
+        public string Query { get; set; } = string.Empty;
+
+        [Description("Anzahl der Ergebnisse (Standard: 5)")]
+        public int Count { get; set; } = 5;
+    }
+
+    protected override async Task<string> ExecuteInternalAsync(Params parameters, ToolContext context)
+    {
+        return await _webSearchProvider.SearchAsync(parameters.Query, parameters.Count);
     }
 }
