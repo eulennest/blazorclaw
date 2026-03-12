baf7f233d4b928dc6b26e8ee0253b3dfb5192d86
Daniel Rieper <git@qdaniel.de>
2026-03-12 17:37:23 +0100

    fix using

--- BlazorClaw.Server/Services/SessionManager.cs
+++ BlazorClaw.Server/Services/SessionManager.cs
@@ -28,7 +28,7 @@ namespace BlazorClaw.Server.Services
             {
                 logger.LogInformation("Creating session {SessionId}", sessionId);
                 var scope = scopeFactory.CreateScope();
-                using var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
                 var sess = await db.ChatSessions.FindAsync(sessionId);
                 model ??= sess?.CurrentModel ?? options.CurrentValue.Model;
                 if (sess == null)
