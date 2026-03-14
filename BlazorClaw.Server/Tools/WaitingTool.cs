98a05c97fc63fbfc13832e2e3a793a484f2d65f2
Daniel Rieper <git@qdaniel.de>
2026-03-14 12:49:10 +0100

    waiting tool

---
+++ BlazorClaw.Server/Tools/WaitingTool.cs
@@ -0,0 +1,33 @@
+using BlazorClaw.Core.Commands;
+using BlazorClaw.Core.Tools;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using BlazorClaw.Core.Services;
+using BlazorClaw.Core.Providers;
+using BlazorClaw.Core.Utils;
+using BlazorClaw.Core.DTOs;
+
+namespace BlazorClaw.Server.Tools;
+
+public class WaitToolParams
+{
+    [Description("Wait Duration in seconds Range: 0 - 120  Default: 1")]
+    [Range(0, 120)]
+    public int? Duration { get; set; } = 1;
+}
+
+public class WaitingTool(ILogger<ImageGenerationTool> logger) : BaseTool<WaitToolParams>
+{
+    public override string Name => "sleep_sec";
+    public override string Description => "Wartet eine bestimmte Anzahl an Sekunden. Kann z.B. verwendet werden um den User eine Nachricht zu senden und danach direkt weiter arbeiten zu können. (z.b. Erstelle dir nun ein schönes Bild.  [Wait_sec] -> [image_gen] -> output)";
+
+    protected override async Task<string> ExecuteInternalAsync(WaitToolParams p, MessageContext context)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(p.Duration??1));
+        return "OK";
+    }
+}
