4803a19abf9feaa312060ffcdd7daf7a70e4c091
Kaspar Kastl <kaspar@eulencode.de>
2026-03-21 20:59:24 +0000

    Register CronJobService in Program.cs

--- BlazorClaw.Server/Program.cs
+++ BlazorClaw.Server/Program.cs
@@ -114,6 +114,10 @@ builder.Services.AddSingleton<ISessionManager, BlazorClaw.Server.Services.Sessio
 builder.Services.AddSingleton<IMessageDispatcher, MessageDispatcher>();
 builder.Services.AddScoped<ISessionQueryService, BlazorClaw.Core.Services.SessionQueryService>();
 
+builder.Services.AddSingleton<CronJobService>();
+builder.Services.AddSingleton<ICronJobService>(sp => sp.GetRequiredService<CronJobService>());
+builder.Services.AddHostedService(sp => sp.GetRequiredService<CronJobService>());
+
 builder.Services.AddHostedService<BlazorClaw.Channels.Services.TelegramBotHostedService>();
 builder.Services.AddHostedService<BlazorClaw.Channels.Services.MatrixBotHostedService>();
 
