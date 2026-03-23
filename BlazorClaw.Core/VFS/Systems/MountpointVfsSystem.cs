214e7e6ab376c7d3ab8cc20a1968ab2b4c5c0abf
Daniel Rieper <git@qdaniel.de>
2026-03-23 01:44:51 +0100

    noop

--- BlazorClaw.Core/VFS/Systems/MountpointVfsSystem.cs
+++ BlazorClaw.Core/VFS/Systems/MountpointVfsSystem.cs
@@ -5,26 +5,31 @@ namespace BlazorClaw.Core.VFS.Systems
 {
     public class MountpointVfsSystem : IVfsSystem
     {
-        public ICollection<KeyValuePair<VfsPath, IVfsSystem>> Mounts { get; private set; }
-
-        public MountpointVfsSystem(IEnumerable<KeyValuePair<VfsPath, IVfsSystem>> mounts)
+        public class MountEntry(IVfsSystem vfs, bool hidden = false)
         {
-            Mounts = new SortedList<VfsPath, IVfsSystem>(new InverseComparer<VfsPath>(Comparer<VfsPath>.Default));
+            public IVfsSystem VFS { get; } = vfs;
+            public bool Hidden { get; } = hidden;
+        }
+        public ICollection<KeyValuePair<VfsPath, MountEntry>> Mounts { get; private set; }
+
+        public MountpointVfsSystem(IEnumerable<KeyValuePair<VfsPath, MountEntry>> mounts)
+        {
+            Mounts = new SortedList<VfsPath, MountEntry>(new InverseComparer<VfsPath>(Comparer<VfsPath>.Default));
             foreach (var mount in mounts)
                 Mounts.Add(mount);
         }
 
-        public MountpointVfsSystem(params KeyValuePair<VfsPath, IVfsSystem>[] mounts)
-            : this((IEnumerable<KeyValuePair<VfsPath, IVfsSystem>>)mounts)
+        public MountpointVfsSystem(params KeyValuePair<VfsPath, MountEntry>[] mounts)
+            : this((IEnumerable<KeyValuePair<VfsPath, MountEntry>>)mounts)
         {
         }
 
-        public void AddMountpoint(VfsPath path, IVfsSystem vfs)
+        public void AddMountpoint(VfsPath path, IVfsSystem vfs, bool hidden = false)
         {
-            Mounts.Add(new KeyValuePair<VfsPath, IVfsSystem>(path, vfs));
+            Mounts.Add(new KeyValuePair<VfsPath, MountEntry>(path, new MountEntry(vfs, hidden)));
         }
 
-        protected KeyValuePair<VfsPath, IVfsSystem>? Get(VfsPath path)
+        protected KeyValuePair<VfsPath, MountEntry>? Get(VfsPath path)
         {
             var t = Mounts.FirstOrDefault(pair => pair.Key == path || pair.Key.IsParentOf(path));
             return t;
@@ -33,12 +38,12 @@ namespace BlazorClaw.Core.VFS.Systems
         public void Dispose()
         {
             foreach (var mount in Mounts.Select(p => p.Value))
-                mount.Dispose();
+                mount.VFS.Dispose();
         }
 
         protected IEnumerable<VfsPath> GetMountPoints(VfsPath path)
         {
-            return [.. Mounts.Where(o => !o.Key.IsRoot && path.Equals(o.Key.ParentPath)).Select(o => o.Key).OrderBy(o => o)];
+            return [.. Mounts.Where(o => !o.Value.Hidden && !o.Key.IsRoot && path.Equals(o.Key.ParentPath)).Select(o => o.Key).OrderBy(o => o)];
         }
 
         public async Task<VfsPathInfo> GetMetaInfoAsync(VfsPath path, CancellationToken cancelationToken = default)
@@ -46,7 +51,7 @@ namespace BlazorClaw.Core.VFS.Systems
             var pair = Get(path);
             if (pair != null)
             {
-                var fi = await pair.Value.Value.GetMetaInfoAsync(path.RemoveParent(pair.Value.Key), cancelationToken);
+                var fi = await pair.Value.Value.VFS.GetMetaInfoAsync(path.RemoveParent(pair.Value.Key), cancelationToken);
                 if (fi != null) return fi.Clone(this, path);
             }
             return new VfsPathInfo(VfsEntity.Create(this, path));
@@ -57,7 +62,7 @@ namespace BlazorClaw.Core.VFS.Systems
             var pair = Get(path);
             if (pair != null)
             {
-                await foreach (var item in pair.Value.Value.GetSubPathsAsync(path.IsRoot ? path : path.RemoveParent(pair.Value.Key), cancelationToken))
+                await foreach (var item in pair.Value.Value.VFS.GetSubPathsAsync(path.IsRoot ? path : path.RemoveParent(pair.Value.Key), cancelationToken))
                 {
                     yield return pair.Value.Key.AppendPath(item);
                 }
@@ -73,7 +78,7 @@ namespace BlazorClaw.Core.VFS.Systems
             var pair = Get(path);
             if (pair != null)
             {
-                await foreach (var item in pair.Value.Value.GetDirectorysAsync(path.IsRoot ? path : path.RemoveParent(pair.Value.Key), cancelationToken))
+                await foreach (var item in pair.Value.Value.VFS.GetDirectorysAsync(path.IsRoot ? path : path.RemoveParent(pair.Value.Key), cancelationToken))
                 {
                     yield return pair.Value.Key.AppendPath(item);
                 }
@@ -89,7 +94,7 @@ namespace BlazorClaw.Core.VFS.Systems
             var pair = Get(path);
             if (pair != null)
             {
-                await foreach (var item in pair.Value.Value.GetFilesAsync(path.IsRoot ? path : path.RemoveParent(pair.Value.Key), cancelationToken))
+                await foreach (var item in pair.Value.Value.VFS.GetFilesAsync(path.IsRoot ? path : path.RemoveParent(pair.Value.Key), cancelationToken))
                 {
                     yield return pair.Value.Key.AppendPath(item);
                 }
@@ -100,31 +105,31 @@ namespace BlazorClaw.Core.VFS.Systems
         {
             if (path.IsRoot) return Task.FromResult(true);
             var pair = Get(path) ?? throw new FileNotFoundException("mountpoint not found", path.ToString());
-            return pair.Value.ExistsAsync(path.RemoveParent(pair.Key), cancelationToken);
+            return pair.Value.VFS.ExistsAsync(path.RemoveParent(pair.Key), cancelationToken);
         }
 
         public Task CreateFileAsync(VfsPath path, Stream data, CancellationToken cancelationToken = default)
         {
             var pair = Get(path) ?? throw new FileNotFoundException("mountpoint not found", path.ToString());
-            return pair.Value.CreateFileAsync(path.RemoveParent(pair.Key), data, cancelationToken);
+            return pair.Value.VFS.CreateFileAsync(path.RemoveParent(pair.Key), data, cancelationToken);
         }
 
         public Task<Stream> OpenFileAsync(VfsPath path, FileMode mode, FileAccess access, CancellationToken cancelationToken = default)
         {
             var pair = Get(path) ?? throw new FileNotFoundException("mountpoint not found", path.ToString());
-            return pair.Value.OpenFileAsync(path.RemoveParent(pair.Key), mode, access, cancelationToken);
+            return pair.Value.VFS.OpenFileAsync(path.RemoveParent(pair.Key), mode, access, cancelationToken);
         }
 
         public Task CreateDirectoryAsync(VfsPath path, CancellationToken cancelationToken = default)
         {
             var pair = Get(path) ?? throw new FileNotFoundException("mountpoint not found", path.ToString());
-            return pair.Value.CreateDirectoryAsync(path.RemoveParent(pair.Key), cancelationToken);
+            return pair.Value.VFS.CreateDirectoryAsync(path.RemoveParent(pair.Key), cancelationToken);
         }
 
         public Task DeleteAsync(VfsPath path, CancellationToken cancelationToken = default)
         {
             var pair = Get(path) ?? throw new FileNotFoundException("mountpoint not found", path.ToString());
-            return pair.Value.DeleteAsync(path.RemoveParent(pair.Key), cancelationToken);
+            return pair.Value.VFS.DeleteAsync(path.RemoveParent(pair.Key), cancelationToken);
         }
     }
 }
---
+++ BlazorClaw.Core/VFS/Systems/NoOpFileSystem.cs
@@ -0,0 +1,55 @@
+namespace BlazorClaw.Core.VFS.Systems
+{
+    public class NoOpFileSystem : IVfsSystem
+    {
+
+        void IDisposable.Dispose()
+        {
+        }
+
+        public IAsyncEnumerable<VfsPath> GetSubPathsAsync(VfsPath path, CancellationToken cancelationToken = default)
+        {
+            return AsyncEnumerable.Empty<VfsPath>();
+        }
+
+        public IAsyncEnumerable<VfsPath> GetDirectorysAsync(VfsPath path, CancellationToken cancelationToken = default)
+        {
+            return AsyncEnumerable.Empty<VfsPath>();
+        }
+
+        public IAsyncEnumerable<VfsPath> GetFilesAsync(VfsPath path, CancellationToken cancelationToken = default)
+        {
+            return AsyncEnumerable.Empty<VfsPath>();
+        }
+
+        public async Task<VfsPathInfo> GetMetaInfoAsync(VfsPath path, CancellationToken cancelationToken = default)
+        {
+             return new VfsPathInfo(VfsEntity.Create(this, path));
+        }
+
+        public Task<bool> ExistsAsync(VfsPath path, CancellationToken cancelationToken = default)
+        {
+            return Task.FromResult(false);
+        }
+
+        public async Task CreateFileAsync(VfsPath path, Stream data, CancellationToken cancelationToken = default)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public Task<Stream> OpenFileAsync(VfsPath path, FileMode mode, FileAccess access, CancellationToken cancelationToken = default)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public Task CreateDirectoryAsync(VfsPath path, CancellationToken cancelationToken = default)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public Task DeleteAsync(VfsPath path, CancellationToken cancelationToken = default)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
